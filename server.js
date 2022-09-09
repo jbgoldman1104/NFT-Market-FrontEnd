2d4dd162ddb768976f419ec13ebc2745403d8336
jbgoldman1104 <jbgoldman1104@gmail.com>
2022-09-08 21:33:32 -0800

    "updated on 2022-09-08"

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   }).listen(3000, (err) => {
